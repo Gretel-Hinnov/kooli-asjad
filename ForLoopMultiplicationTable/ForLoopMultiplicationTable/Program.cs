7a76e8d8dc93bb6a0208df9e847d60174849b02e
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-01-08 12:46:56 +0200

    fix1

--- ForLoopMultiplicationTable/ForLoopMultiplicationTable/Program.cs
+++ ForLoopMultiplicationTable/ForLoopMultiplicationTable/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Korrutustabel");
+            int j, i, n;
+
+            Console.WriteLine("sisesta tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("korrutustabel ühest kuni {0}", n);
+            Console.WriteLine("\n");
+
+            for (i= 1; i < 10; i++) 
+            { 
+                for (j =1; j <= n; j++)
+                { 
+
+                    if (j <= n-1)
+                    {
+                        Console.Write("{0}x{1} = {2}", j, i , i* j);
+                    }
+                    else
+                    {
+                        Console.Write("{0}x{1} = {2}", j, i, i * j);
+                    }
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
