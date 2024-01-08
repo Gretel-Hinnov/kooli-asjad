2ccfcca876547297040871d48815fe6996a099ed
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-01-08 13:16:01 +0200

    triangle

---
+++ ForLoopTriangle/ForLoopTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ForLoopTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tärnidest tehtud kolmnurk e poolik püramiid:");
+            Console.WriteLine("-----------------------------------------------");
+
+            
+            int val = 10;
+            Console.Write("Sisesta ridade arv : ");
+            val = Convert.ToInt32(Console.ReadLine());
+            int i, j, k;
+            for (i = 1; i <= val; i++)
+            {
+                for (j = 1; j <= val - i; j++)
+                {
+                    // Console.Write("");  
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
