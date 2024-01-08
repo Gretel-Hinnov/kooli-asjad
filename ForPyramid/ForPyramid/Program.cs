ee1a721af4c11ea57008974a440b4c017c87e428
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-01-08 13:57:42 +0200

    Pyramid

---
+++ ForPyramid/ForPyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ForPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int i, j, n;
+
+            Console.Write("Loo püramiid tärnidest : \n");
+            Console.Write("----------------------------");
+            Console.Write("\n\n");
+            Console.WriteLine("Sisiesta püramiidi suurus");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i= 0; i < n; i++) 
+            {  
+                for (j = 1; j <= n-i; j++) 
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2*i-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
