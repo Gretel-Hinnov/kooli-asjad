705fe09d651c2ce472ce56f078ca887013866f9e
Opilane TTHK <opilane@tthk.ee>
2024-01-11 13:53:31 +0200

    Square

---
+++ ForLoopSquare/ForLoopSquare/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ForLoopSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ruut");
+            Console.WriteLine("Sisesta ruudu suurus");
+            int size = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row < size; row++)
+            {
+                for (int column = 1; column <= size; column++)
+                {
+                    string mark;
+
+                    if (row == column || row + column <= size + 1)
+                    {
+                        mark = "*";
+                    }
+                    else
+                    {
+                        mark = "*";
+                    }
+                    Console.Write(mark);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
