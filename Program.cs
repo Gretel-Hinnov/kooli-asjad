b8c198b4fa95f29be6e76f25f41f7a0d9858010c
Opilane TTHK <opilane@tthk.ee>
2024-02-16 13:34:43 +0200

    DictionaryForeach

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DictionaryForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Dictionary Foreach");
+            var domains = new Dictionary<string, string>()
+            {
+                {"est -", "Estonia"},
+                {"lat -", "Latvia"},
+                {"fin -", "Finland"},
+                {"swe -", "Sweden"}
+            };
+           
+            foreach (var domain in domains )
+            {
+                Console.WriteLine($" {domain.Key}- {domain.Value} - {i}");
+                i++;
+            }
+        }
+    }
+}
