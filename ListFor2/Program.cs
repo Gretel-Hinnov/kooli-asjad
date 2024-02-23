a366288ccc5e0520f4de9e2c5e87feec442d3035
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-02-23 12:04:41 +0200

    ListFor

---
+++ ListFor2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ListFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+
+            Console.WriteLine("Number of elements: " + primeNumbers.Count);
+            Console.WriteLine();
+
+            var cities = new List<string>();
+            cities.Add("New York");
+            cities.Add("London");
+            cities.Add("Mumbai");
+            cities.Add("Rome");
+            cities.Add(null);
+
+            Console.WriteLine("Number of elements: " + cities.Count);
+        }
+    }
+}
