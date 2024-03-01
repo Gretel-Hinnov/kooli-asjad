59387d41f5c3e849209971bc715bb961c84c8592
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-03-01 13:08:55 +0200

    ArraySortNames

---
+++ ArraySortNames/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ArraySortNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = { "cat", "alligator", "fox", "donkey", "bear", "elephant", "goat" };
+
+           //Array.Sort(animals);
+
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            Array.Sort(animals, 0, 3);
+
+            foreach (var animal in animals )
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("--------------------------------");
+
+            int[] numbers = {1, 2, 3, 4, 3, 55, 23, 2 };
+            IEnumerable<int> distinctNumbers = numbers.Distinct().ToList();
+
+            foreach (int number in distinctNumbers)
+            {
+                Console.WriteLine(number + " ");
+            }
+        }
+    }
+}
