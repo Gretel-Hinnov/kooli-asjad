f736e8c95a33b24abd88d35004d09e7b275bcb35
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-03-01 12:19:27 +0200

    RandomAsterisk

---
+++ RandomAsterisk/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RandomAsterisk_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stringRowMin = 0;
+            int stringRowMax = 5;
+
+            int[] randomAsterisk = new int[stringRowMax];
+
+            Random asterisk = new Random();
+
+            for (int i = stringRowMin; i < randomAsterisk.Length; i++)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*",
+                    asterisk.Next(1,20))));
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (int i in randomAsterisk)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*",
+                  asterisk.Next(1, 20))));
+            }
+        }
+    }
+}
