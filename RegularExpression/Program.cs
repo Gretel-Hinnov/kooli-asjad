246db6afb70ef639b19ac43fa05ce969be1ed860
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-03-01 14:01:37 +0200

    RegularExpression

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "#CD5C5C";
+            Console.WriteLine("Hex code: " + word);
+            Console.WriteLine("kas on regex: " + RegExTest(word));
+        }
+
+        public static bool RegExTest (string word)
+        {
+            return Regex.IsMatch (word, @"[#] [0-9A-Fa-f] {6} \b");
+        }
+    }
+}
