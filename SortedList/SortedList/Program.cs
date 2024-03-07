69c13ea4de3c69e5c26e1f3c6daed24a6520198b
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-03-07 09:47:30 +0200

    SortedList

---
+++ SortedList/SortedList/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorted list");
+
+            SortedList<int, string> numberNames = new SortedList<int, string>();
+
+            numberNames.Add(3, "Three");
+            numberNames.Add(1, "One");
+            numberNames.Add(2, "Two");
+            numberNames.Add(4, null);
+            numberNames.Add(10, "Ten");
+            numberNames.Add(5, "Five");
+
+            foreach (var number in numberNames)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", number.Key, number.Value);
+            }
+
+
+        }
+    }
+}
