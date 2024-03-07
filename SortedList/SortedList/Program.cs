d2def221256f2ca1554497b7858214c31a52fd80
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-03-07 10:04:47 +0200

    SortedList2

--- SortedList/SortedList/Program.cs
+++ SortedList/SortedList/Program.cs
@@ -20,7 +20,28 @@
                 Console.WriteLine("Key: {0}, Value: {1}", number.Key, number.Value);
             }
 
+            Console.WriteLine("--------------------------------------------------");
+            foreach (KeyValuePair<int, string> a in numberNames) 
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", a.Key, a.Value);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Numbrite eemaldamine");
+            SortedList<int, string> keyValuePairs = new SortedList<int, string>()
+            {
+                {3,"Three" },
+                {1,"One" },
+                {2,"Two" },
+                {5,"Five" },
+                {4,"Four" },
+            };
+            keyValuePairs.Remove(2);
+            keyValuePairs.RemoveAt(0);
 
+            foreach (var kvp in keyValuePairs)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            }
         }
     }
 }
