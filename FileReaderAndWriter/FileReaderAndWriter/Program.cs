60259cd2345821a1fabd387e61b99dadbec1e239
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-03-08 13:50:11 +0200

    FileReaderAndWriter

---
+++ FileReaderAndWriter/FileReaderAndWriter/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace FileReaderAndWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            FileReaderAndWriter();
+        }
+
+
+        static void FileReaderAndWriter()
+        {
+            string line; 
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/tuttavad.txt"))
+                {
+
+                    string rida = sr.ReadToEnd();
+                        string[] nimed = rida.Split('\n');
+
+                        Array.Sort(nimed);
+
+                        foreach (var nimi in nimed)
+                        {
+                        Console.WriteLine(nimi);
+                        }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Faili ei saa lugeda");
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+            using (StreamWriter sr = new StreamWriter("C:/Users/opilane/Desktop/tuttavad.txt"))
+            {
+
+            }
+                
+
+        }
+
+    }
+}
