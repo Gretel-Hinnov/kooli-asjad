d340b0c377081d0c91c6c67678aecfda76e5286f
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-05-10 14:00:14 +0300

    Car VIN code

---
+++ Car VIN code/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Reflection.PortableExecutable;
+
+namespace InheritanceVINCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta masina VIN kood: ");
+            int VIN = Convert.ToInt32(Console.ReadLine());
+
+            Machine machine = new Machine();
+            machine.Cars(VIN);
+            Console.WriteLine("edukalt sisestatud " + machine.CarCode());
+        }
+    }
+    class Car
+    {
+        public void Cars(int v)
+        {
+            vin = v;
+        }
+        protected int vin;
+    }
+    class Machine : Car
+    {
+        public int CarCode()
+        {
+            return vin;
+        }
+    }
+}
+
