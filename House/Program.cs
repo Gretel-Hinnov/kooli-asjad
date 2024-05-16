d2bd6e67766f57baccdacd8a07f70024a2e8cd10
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-05-16 12:57:46 +0300

    House

---
+++ House/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House();
+        }
+        static void House()
+        {
+
+            int size = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= size - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i * 2 - 1; j++)
+                {
+                    Console.Write("_");
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = 1; row < size; row++)
+            {
+                for (int column = 1; column <= size * 2; column++)
+                {
+                    string mark;
+
+                    if (row == column || row + column <= size + 1)
+                    {
+                        mark = "|";
+                    }
+                    else
+                    {
+                        mark = "|";
+                    }
+                    Console.Write(mark);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    
+
+}
+
+
