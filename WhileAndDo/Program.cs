98dc9001f84e14c916ec41f91d77f952b3d3b70a
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-05-16 12:44:56 +0300

    WhileAndDo

---
+++ WhileAndDo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WhileAndDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+             Console.WriteLine("i = {0}", i);
+                i++;
+            } while (i < 20);
+        }
+    }
+}
