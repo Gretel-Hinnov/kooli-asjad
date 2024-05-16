57d5b8c49eab5d45d804d837ee5e0b5fb15c694e
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-05-16 11:08:15 +0300

    Switch harjutus

---
+++ Switch harjutus/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace harjutus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("kirjuta number");
+            int boop = int.Parse(Console.ReadLine());
+
+
+
+            if (boop == 30)
+            {
+                Jah();
+                Console.WriteLine(boop);
+            }
+            else
+            {
+                Console.WriteLine("ei :(");
+            }
+        
+        }
+        static void Jah()
+        {
+            int boop = 30;
+            switch (boop)
+            {
+                case 1:
+                    Console.WriteLine("esimene");
+                    break;
+                case 2:
+                    Console.WriteLine("teine");
+                    break;
+                case 3:
+                    Console.WriteLine("kolmas");
+                    break;
+                default:
+                    Console.WriteLine("neljas");
+                    break;
+            }
+        }
+    }
+}
