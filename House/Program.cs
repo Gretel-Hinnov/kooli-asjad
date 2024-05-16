585af44bafb8347b97de4c11816d9389db6da604
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-05-16 14:11:17 +0300

    Diamond

--- House/Program.cs
+++ House/Program.cs
@@ -6,7 +6,9 @@ namespace ForLoop
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("kirjuta mingi number");
             House();
+            Diamond();
         }
         static void House()
         {
@@ -43,10 +45,45 @@ namespace ForLoop
                 }
                 Console.WriteLine();
             }
+
+        }
+        static void Diamond()
+        {
+            int i, j, n;
+            Console.WriteLine("Sisiesta püramiidi suurus");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+            for (i = n - 1; i >= 1; i--)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+
+            }
         }
     }
+}
     
 
-}
+
 
 
