b00879757dd9e391a143db7aebd6018a5e900795
Opilane TTHK <150146569+Gretel-Hinnov@users.noreply.github.com>
2024-05-16 09:14:06 +0300

    IfandElse+vooskeem

---
+++ IfandElse/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace KordamineIfanElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("numbrid");
+            int numbrid = 12;
+
+            if (numbrid > 4)
+            {
+                Console.WriteLine("12 on suurem kui 4");
+            }
+            else
+            {
+                Console.WriteLine("vale");
+            }
+        
+        }
+    }
+}
